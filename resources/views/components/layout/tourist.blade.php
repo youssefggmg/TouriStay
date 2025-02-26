5ac1f9facb5848155283af0979ff7133dee6f5aa
youssef elhoubi <elhoubiyoussef@gmail.com>
2025-02-26 01:27:46 +0000

    made the front of the user and stell need some changde and te be implemented the back end

--- resources/views/components/layout/tourist.blade.php
+++ resources/views/components/layout/tourist.blade.php
@@ -2,7 +2,8 @@
 <html>
 
 <head>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.2.19/tailwind.min.js"></script>
+    {{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.2.19/tailwind.min.js"></script> --}}
+    <script src="https://unpkg.com/@tailwindcss/browser@4"></script>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
     <title>{{ config('app.name', 'Laravel') }}</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
--- resources/views/tourist/home.blade.php
+++ resources/views/tourist/home.blade.php
@@ -1,3 +1,15 @@
 <x-layout.tourist>
-    
+    <x-tourist.touristHeader></x-ltourist.touristHeader>
+        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+            <div class="flex flex-col lg:flex-row">
+                {{-- sidebar --}}
+                <x-tourist.touristSidebar></x-tourist.touristSidebar>
+                {{-- mian content --}}
+                <div class="flex-1">
+                    <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 flex-nowrap">
+                        <x-tourist.touristCard></x-tourist.tourist-card>
+                    </div>
+                </div>
+            </div>
+        </div>
 </x-layout.tourist>
